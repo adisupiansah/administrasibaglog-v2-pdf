597f22d1359dcfa4cc5927b3502cb35ec0bb2b26
adisupiansah <adisupiansyah15@gmail.com>
2024-12-31 11:26:49 +0700

    Update 31-12-2024

--- prisma/migrations/20241231042602_db_silogad/migration.sql
+++ prisma/migrations/20241231042602_db_silogad/migration.sql
@@ -53,8 +53,9 @@ CREATE TABLE `notadinas` (
 -- CreateTable
 CREATE TABLE `user` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(100) NOT NULL,
     `password` VARCHAR(200) NOT NULL,
+    `username` VARCHAR(200) NOT NULL,
 
+    UNIQUE INDEX `username`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
